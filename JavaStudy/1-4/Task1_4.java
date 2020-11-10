4cdce2922867e86daef800bd2be997dc7b91e496
kyousuke <pimao7828@icloud.com>
2020-11-10 22:06:47 +0900

    制御文　課題再提出
    Task1_4.java 修正

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -13,8 +13,9 @@ public class Task1_4 {
 
     // 定数（アカウント情報）
     // ① 登録されている名前とパスワードを定数で定義してください。
-
-
+    
+    private static final String NAME ="alice";
+    private static final String PSSS ="pass";
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
     private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
@@ -33,17 +34,17 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if(name.equals("alice") && pass.equals("pass")){
+        if(name.equals(name) && pass.equals(pass)){
             System.out.println(CONST_MSG_SUCCESS);
         }
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        else if(name.equals("alice")){
+        else if(name.equals(name)){
             System.out.println(CONST_MSG_ERROR_PASS);
         }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        else if(pass.equals("pass")){
+        else if(pass.equals(pass)){
             System.out.println(CONST_MSG_ERROR_NAME);
         }
 
