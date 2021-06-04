784c3e3a1d671d769a49c50db8826198e1588858
kyousuke <pimao7828@icloud.com>
2021-06-04 20:55:23 +0900

    Check1課題提出
    Check1/index.html main.js 作成

--- JavaScript/Cheack1/main.js
+++ JavaScript/Cheack1/main.js
@@ -1,6 +1,31 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 //ここに答えを実装してください。↓↓↓
-console.log(numbers);
 function isEven() {
-    console.log(num+ 'は偶数です');
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        num = numbers[i];
+        console.log(num + "は偶数です");
+    }
 }
+}
+isEven();
+
+
+//問２//
+
+//Carクラスの作成
+class Car {
+    //コンストラクタ
+constructor(gass, num) {
+  this.gass = gass;
+  this.num = num;
+}
+
+getNumGas() {
+  console.log(`ガソリンは${this.gass}です`);
+  console.log(`ナンバーは${this.num}です`);
+}
+}
+//carオブジェクト作成　引数がコンストラクタに代入されgetNumGasメソッドで表示される
+let car = new Car(20.5, 1234);
+car.getNumGas();
